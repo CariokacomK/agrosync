c1f135ce22468e2a9b34cd35b5cd24a011c8f83c
Gabriel Borges <gobcarioka@gmail.com>
2025-09-15 16:57:49 -0300

    add module usuario

--- agrosync-web/src/layouts/DashboardLayout.tsx
+++ agrosync-web/src/layouts/DashboardLayout.tsx
@@ -4,9 +4,9 @@ import Sidebar from '../components/Sidebar';
 
 const DashboardLayout: React.FC = () => {
     return (
-        <div style={{ display: 'flex' }}>
+        <div style={{ display: 'flex', minHeight: '100vh', backgroundColor: '#f5f5f5' }}>
             <Sidebar />
-            <main style={{ flexGrow: 1, padding: '20px' }}>
+            <main style={{ flexGrow: 1, padding: '20px', display: 'flex',flexDirection: 'column' }}>
                 <Outlet /> 
             </main>
         </div>
